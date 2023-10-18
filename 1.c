0e24e9eb13ccc2d9f8d802aff01f78c87d8cd489
Anas Ibn Belal <98760910+anas20023@users.noreply.github.com>
2023-10-19 00:31:03 +0600

    one

--- 1.c
+++ 1.c
@@ -5,4 +5,17 @@ int main()
     int x;
     printf("Enter Aray Size:");
     scanf("%d",&x);
+    int arr[x];
+    int i,sum=0;
+    for(i=0;i<x;i++)
+    {
+        printf("Enter %d Element:",i+1);
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<x;i++)
+    {
+        sum+=arr[i];
+    }
+    printf("Sum of Array Elements:%d",sum);
+
 }
