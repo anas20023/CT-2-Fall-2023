7d4d9f3c47c99299e8fffccac90903d797478fd4
Anas Ibn Belal <98760910+anas20023@users.noreply.github.com>
2023-10-21 13:22:37 +0600

    lwr to uprcase

---
+++ 10.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int Count_len(char *ch)
+{
+    int i, x = 0;
+    for (i = 0; ch[i] != '\0'; i++)
+    {
+        x++;
+    }
+    return x;
+}
+
+int main()
+{
+    int i, j;
+    char ch[300];
+    char ch_2[300];
+    printf("Enter Lowercase String:");
+    gets(ch);
+
+    int x = Count_len(ch);
+
+    for (i = 0; i < x; i++)
+    {
+        if (ch[i] >= 'a' && ch[i] <= 'z')
+        {
+            ch_2[i] = ch[i] - 32;
+        }
+
+        else
+        {
+
+            ch_2[i] = ch[i];
+        }
+    }
+    ch_2[i] = '\0';
+
+    printf("Uppercase String is %s\n", ch_2);
+}
