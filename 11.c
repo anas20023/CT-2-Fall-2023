ddbf1e437dc18f51291b985bf5e511c211be4804
Anas Ibn Belal <98760910+anas20023@users.noreply.github.com>
2023-10-21 16:21:54 +0600

    space remove

--- 11.c
+++ 11.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 
+int Count_Lenth(char *ch)
+{
+    int i = 0;
+    while (ch[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+
 int main()
 {
     char anas[300];
     printf("Enter String With Space:");
-    scanf("%[^\n]",&anas);
-    
-    for (int i = 0; i < count; i++)
-    {
-        /* code */
-    }
-    
+    scanf("%[^\n]", &anas);
+    int x = Count_Lenth(anas);
 
+    for (int i = 0; i < x; i++)
+    {
+        if (anas[i] == ' ')
+        {
+            for (int j = i; j < x; j++)
+            {
+                anas[j] = anas[j + 1];
+            }
+            x--;
+        }
+    }
+    printf("String Without Space: %s", anas);
 }
